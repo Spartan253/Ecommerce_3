b8b22038607103092457c4aaf84359d64ba7c9bf
Spartan 253 <ayushkhoke1@gmail.com>
2026-01-02 12:08:44 +0530

    Fix CORS for Vercel frontend

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT=process.env.PORT || 4000;
 database.connect();
 
 app.use(cors({
-  origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+  origin: [ "https://ecommerce-3-tau.vercel.app"],
   credentials: true,
 }));
 
